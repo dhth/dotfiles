dd4571ee2487cb8de38b24813a66288c67b0230d
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2026-02-07 13:46:10 +0100

    karabiner, tmux

--- tmux/tmux-switch-session.sh
+++ tmux/tmux-switch-session.sh
@@ -1,7 +1,7 @@
 function txs() {
     local selected_entry
 
-    selected_entry=$(tmux list-sessions -F "#{session_last_attached} #{session_name}" | sort -nr | awk '{print $2}' | fzf --height=20 --layout=reverse --header="session?")
+    selected_entry=$(tmux list-sessions -F "#{?session_last_attached,#{session_last_attached},#{session_created}} #{session_name}" | sort -nr | awk '{print $2}' | fzf --height=20 --layout=reverse --header="session?")
 
     if [ -n "$selected_entry" ]; then
         tmux switch-client -t $selected_entry
